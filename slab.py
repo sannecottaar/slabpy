198b536ce127d96cb1068a2fad427758744127a2
Sanne Cottaar <sanne@Sannes-MacBook-Pro.local>
2016-07-11 10:56:49 -0700

    first file

---
+++ slab.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import numpy as np
+
+
+
+class Slabs(object):
+
+    """
+    Base class for all slabs.
+
+    """
+
+    def __init__(self,name):
+        self.name = None
+        self.shape = None
+        self.seismicity = None
+        self.slabneighbors = None
+
+
+
+    def plot_tomography(model):
+        '''
+        Plot slab cross section in given model.
+        Potential model names are : ???
+        '''
+        pass
+
+    def plot_seismicity(model):
+        '''
+        Plot slab cross section in given model.
+        Potential model names are : ???
+        '''
+        pass
+
+
+
+
+
+
+
+
