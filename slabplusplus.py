f9ff9925a466afdd525b7d987992e328f71ead6a
TOLULOPE OLUGBOJI <tolumorayo@gmail.com>
2016-07-15 14:55:50 -0400

    added code to compute temperature in slab at depth of 600 km:

--- slabplusplus.py
+++ slabplusplus.py
@@ -33,7 +33,14 @@ def read_excel(file='slab++.xlsx'):
                 slab.params[par]= float(value)
             except:
                 slab.params[par] = str(value)
-
+                
+        # save temperature at 600 km using thermal parameter
+        phi = slab.params['thermalPar']
+        z = 6.0
+        Ta = 1338.0
+        Tz = Ta * (1.0 - ( (2.0/np.pi)*np.exp(-1.0*( (np.power(np.pi,2.0)*z)/ (np.power(2.32,2.0) * phi) )) ) )
+        slab.params['Temp600'] = Tz
+        print(Tz)
         print(slab.params)
         slabs.append(slab)
     return slabs
@@ -41,10 +48,16 @@ def read_excel(file='slab++.xlsx'):
 # Read and plot Syracuse Catalog
 slabs = read_excel()
 print(slabs.list('Fukao'))
-plt.plot(slabs.list('Fukao'), slabs.list('thermalPar'),'.k')
+print(slabs.list('thermalPar'))
+#plt.plot(slabs.list('Fukao'), slabs.list('thermalPar'),'.k')
+
+plt.plot(slabs.list('Fukao'), slabs.list('Temp600'),'.k')
+print( slabs.list('Temp600') )
+
 plt.xlim([0,6.0])
 plt.xlabel('Fukao categories')
 plt.xticks([1,2,3,4,5],['above 660','through 660', 'above 1000', 'deep','uncateg.'])
-plt.ylabel('thermal parameter')
+#plt.ylabel('thermal parameter')
+plt.ylabel('temperature (k)')
 plt.show()
 
